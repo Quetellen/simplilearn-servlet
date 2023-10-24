82600b044dd61150f442be0d6486dc37ae7fdeea
Quetellen <quetellen.bertazo@gmail.com>
2023-10-24 09:30:11 -0700

    saving files

--- src/main/java/com/simplilearn/course/servlet/_09_DBServlet.java
+++ src/main/java/com/simplilearn/course/servlet/_09_DBServlet.java
@@ -16,19 +16,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import com.simplilearn.course.bean.User;
 
